2c9251831083cabc4a87366fb486aa2098bd279e
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2024-02-11 13:58:07 +0200

    refactor: table for contrast ratios

--- notebooks/experiments/contrast_ratio.clj
+++ notebooks/experiments/contrast_ratio.clj
@@ -1,4 +1,5 @@
-^{:nextjournal.clerk/visibility {:code :hide}}
+{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+
 (ns experiments.contrast-ratio
   (:require [clojure.math]
             [garden.color :as c]
@@ -25,14 +26,15 @@
 
 ;; E.g. for sRGB, relative luminance can be calculated from linear RGB components.
 
-^{::clerk/visibility {:result :hide}}
 (defn rgb->srgb [{:keys [red green blue]}]
   {:red (/ red 255.0)
    :green (/ green 255.0)
    :blue (/ blue 255.0)})
 
+^{:nextjournal.clerk/visibility {:code :show :result :show}}
 (c/hex->rgb "#6056EB")
 
+^{:nextjournal.clerk/visibility {:code :show :result :show}}
 (-> (c/hex->rgb "#6056EB")
     rgb->srgb)
 
@@ -43,24 +45,22 @@
 ;; - if GsRGB <= 0.03928 then G = GsRGB/12.92 else G = ((GsRGB+0.055)/1.055) ^ 2.4
 ;; - if BsRGB <= 0.03928 then B = BsRGB/12.92 else B = ((BsRGB+0.055)/1.055) ^ 2.4
 
-^{::clerk/visibility {:result :hide}}
 (defn curve-fn-1 [d]
   (/ d 12.92))
 
-^{::clerk/visibility {:result :hide}}
 (defn curve-fn-2 [d]
   (-> d
       (+ 0.055)
       (/ 1.055)
       (clojure.math/pow 2.4)))
 
-^{::clerk/visibility {:result :hide}}
 (defn gamma-transfer-function [d]
   (let [threshold 0.04045] ; Before May 2021 the value of 0.04045 in the definition was different (0.03928).
     (if (<= d threshold)
       (curve-fn-1 d)
       (curve-fn-2 d))))
 
+^{:nextjournal.clerk/visibility {:code :show :result :show}}
 (-> (c/hex->rgb "#6056EB")
     rgb->srgb
     (update :red gamma-transfer-function)
@@ -76,7 +76,6 @@
 ;; The formula reflects the luminous efficiency function as "green" light is the major component of luminance,
 ;; responsible for the majority of light perceived by humans, and "blue" light the smallest component.
 
-^{::clerk/visibility {:result :hide}}
 (defn get-cie-y [srgb]
   (let [expand-multiplier #(comp (partial * %) gamma-transfer-function)]
     (-> srgb
@@ -84,11 +83,11 @@
         (update :green (expand-multiplier 0.7152))
         (update :blue (expand-multiplier 0.0722)))))
 
-^{::clerk/visibility {:result :hide}}
 (defn relative-luminance [color]
   (let [{:keys [red green blue]} (get-cie-y (rgb->srgb color))]
     (+ red green blue)))
 
+^{:nextjournal.clerk/visibility {:code :show :result :show}}
 (mapv (comp relative-luminance c/hex->rgb)
       ["#FFFFFF" ; white
        "#000000" ; black
@@ -100,14 +99,15 @@
 
 ;; (L1 + 0.05) / (L2 + 0.05)
 
-^{::clerk/visibility {:result :hide}}
 (defn get-contrast-ratio [color color-dark]
   (let [L1 (relative-luminance color)
         L2 (relative-luminance color-dark)]
     (/ (+ L1 0.05)
        (+ L2 0.05))))
 
-(list (get-contrast-ratio (c/hex->rgb "#FFFFFF") (c/hex->rgb "#000000")) ; white-black 
-      (get-contrast-ratio (c/hex->rgb "#FFFFFF") (c/hex->rgb "#6056EB")) ; white-primary
-      (get-contrast-ratio (c/hex->rgb "#6056EB") (c/hex->rgb "#000000")) ; primary-black
-      )
+^{:nextjournal.clerk/visibility {:result :show}}
+(clerk/table
+ {:head ["Light color" "Dark color" "Contrast ratio"]
+  :rows (list ["#FFFFFF (white)" "#000000 (black)" (get-contrast-ratio (c/hex->rgb "#FFFFFF") (c/hex->rgb "#000000"))]
+              ["#FFFFFF (white)" "#6056EB (primary)" (get-contrast-ratio (c/hex->rgb "#FFFFFF") (c/hex->rgb "#6056EB"))]
+              ["#6056EB (primary)" "#000000 (black)" (get-contrast-ratio (c/hex->rgb "#6056EB") (c/hex->rgb "#000000"))])})
